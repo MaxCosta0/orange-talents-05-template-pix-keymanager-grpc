e7db811a21be3a775dfb2af79ff33bc78318a8ed
MaxCosta0 <maxleysoares006@gmail.com>
2021-07-07 13:31:02 -0300

    Setup Projeto

---
+++ src/main/kotlin/br/com/zupacademy/maxley/Application.kt
@@ -0,0 +1,9 @@
+package br.com.zupacademy.maxley
+
+import io.micronaut.runtime.Micronaut.*
+fun main(args: Array<String>) {
+	build()
+	    .args(*args)
+		.packages("br.com.zupacademy.maxley")
+		.start()
+}
